7c5ec64eb9e5b480647c5a1e0888832ba8222b1f
Gaurav Bhalekar <gauravbhalekar5@gmail.com>
2023-03-20 09:33:28 +0530

    Jenkins Master and Slave Configuration

---
+++ Jenkins Master and Slave Configuration.sh
@@ -0,0 +1,16 @@
+Jenkins Master and Slave Configuration:
+
+Important Topic:
+
+On Master Node:
+# yum update -y
+# yum install java-11* -y
+# find /usr/lib/jvm/java-11* | head -n 3
+
+
+
+
+On Slave Node:
+# yum update -y
+# yum install java-11* -y
+# find /usr/lib/jvm/java-11* | head -n 3
